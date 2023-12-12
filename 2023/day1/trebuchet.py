1dbb1d4d102e3a4b8f355fb0291649c90f956bc7
recursory <olivianaomilund@gmail.com>
2023-12-12 03:38:59 -0800

    finished day 1 part 2

--- 2023/day1/trebuchet.py
+++ 2023/day1/trebuchet.py
@@ -5,21 +5,35 @@ try:
 except:
     print("error: failure to open input file specified on command line")
 
+numbers = [
+        ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+        ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+]
+
 t = 0
 for line in f:
-    #print(line)
+    line = line.replace('\n', '')
+    rline = line[::-1]
+    for i in range(0, len(line)-3): 
+        for numlen in range(3, 6):
+            if len(line) - i + 1> numlen:
+                for j in range(0, 10):
+                    if numbers[0][j] in line[i:i+numlen]:
+                        line = line.replace(numbers[0][j], numbers[1][j], 1)
+    for i in range(0, len(rline)-3): 
+        for numlen in range(3, 6):
+            if len(rline) - i + 1> numlen:
+                for j in range(0, 10):
+                    if numbers[0][j][::-1] in rline[i:i+numlen]:
+                        rline = rline.replace(numbers[0][j][::-1], numbers[1][j], 1)
+
     for cha in line:
-        #print(f'is {cha} alphanumeric?')
         if cha.isnumeric():
-            #print('yes')
             fir = cha
             break
-    for cha in reversed(line):
-        #print(f'is {cha} alphanumeric?')
+    for cha in rline:
         if cha.isnumeric():
-            #print('yes')
             sec = cha
             break
-    #print(f'{fir} + {sec} = {10*int(fir)+int(sec)}')
     t = t + 10 * int(fir) + int(sec)
 print(t)
